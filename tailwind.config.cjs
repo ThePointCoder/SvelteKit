5259c6e0dc2ac15be4c4a806f3e1a721ef93ac3c
ThePointCoder <marta.janos82@gmail.com>
2022-01-19 18:36:28 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  media: false,
+  theme: {
+    fontFamily: {
+      'quicksand': ['Mochiy Pop P One', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
